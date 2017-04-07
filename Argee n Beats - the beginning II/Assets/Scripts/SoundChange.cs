c0b69cc2fd0347875d19fe77191b8f9f7460df8f
Jawsarn <christian_markowicz@hotmail.com>
2017-04-07 21:58:06 +0200

    Stuff

--- Argee n Beats - the beginning II/Assets/Scripts/SoundChange.cs
+++ Argee n Beats - the beginning II/Assets/Scripts/SoundChange.cs
@@ -4,19 +4,49 @@ using UnityEngine;
 
 public class SoundChange : MonoBehaviour {
 
-    RenderTexture renderTexture;
+    //RenderTexture renderTexture;
     Texture2D texture;
+    public float radius;
 
 	// Use this for initialization
 	void Start () {
         //renderTexture = new RenderTexture(32, 32, 0, RenderTextureFormat.ARGB32);
         //renderTexture.Create();
+        SoundChangeManager.scManager.Register(this);
+
         texture = new Texture2D(32,32);
         GetComponent<Renderer>().material.mainTexture = texture;
     }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+
+    }
+
+    public void ColorByPosition(Vector3 pos)
+    {
+        for (int x = 0; x < 32; x++)
+        {
+            for (int y = 0; y < 32; y++)
+            {
+                int val = Random.Range(0, 10);
+                if (val < 5)
+                {
+                    texture.SetPixel(0, 0, Color.blue);
+                }
+                else
+                {
+                    texture.SetPixel(0, 0, Color.red);
+                }
+            }
+        }
+
+
+        texture.Apply();
+    }
+
+    void OnDestroy()
+    {
+        SoundChangeManager.scManager.Unregister(this);
+    }
 }
---
+++ Argee n Beats - the beginning II/Assets/Scripts/SoundChangeManager.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundChangeManager : MonoBehaviour {
+
+    public static SoundChangeManager scManager = null;
+
+    List<SoundChange> soundChangers = new List<SoundChange>();
+    int counter = 0;
+
+    void Awake()
+    {
+        if (scManager)
+        {
+            scManager = this;
+        }
+        else
+        {
+            Debug.LogError("MULTIPLE SINGLETON");
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    bool CheckInRange(SoundChange sc)
+    {
+        return true;
+    }
+
+	// Update is called once per frame
+	void Update () {
+
+        // Update current
+        if (counter < soundChangers.Count)
+        {
+            SoundChange sc = soundChangers[counter];
+
+            // check if in range
+            if (CheckInRange(sc))
+            {
+                sc.ColorByPosition(transform.position);
+            }
+        }
+        
+
+        counter++;
+        if (counter > soundChangers.Count)
+        {
+            counter = 0;
+        }
+    }
+
+    public void Register(SoundChange sc)
+    {
+        soundChangers.Add(sc);
+    }
+
+    public void Unregister(SoundChange sc)
+    {
+        soundChangers.Remove(sc);
+    }
+}
