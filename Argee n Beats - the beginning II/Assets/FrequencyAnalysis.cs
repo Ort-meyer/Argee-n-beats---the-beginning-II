e8032ba0908aceeef786ffc2279fe39051203e04
Acc <Acc>
2017-04-07 21:30:50 +0200

    work in progress frequency analysis

--- Argee n Beats - the beginning II/Assets/FrequencyAnalysis.cs
+++ Argee n Beats - the beginning II/Assets/FrequencyAnalysis.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 
 public class FrequencyAnalysis : MonoBehaviour
 {
-
+    private bool didOnce;
     private AudioSource m_source;
     // Use this for initialization
     void Start()
     {
+        didOnce = false;
         AudioSource source = GetComponent<AudioSource>();
         source.clip = Microphone.Start("", false, 10, 44100);
 
@@ -19,17 +20,43 @@ public class FrequencyAnalysis : MonoBehaviour
     {
         if (Input.GetKeyUp(KeyCode.G))
         {
-            //GetComponent<AudioSource>().Play();
-            float[] spectrum = new float[256];
-            m_source.GetSpectrumData(spectrum, 0, FFTWindow.Rectangular);
-            
-            for (int i = 1; i < spectrum.Length - 1; i++)
+            ////GetComponent<AudioSource>().Play();
+            //float[] spectrum = new float[256];
+            //GetComponent<AudioSource>().GetSpectrumData(spectrum, 0, FFTWindow.Rectangular);
+            //
+            //float max = 0;
+            //for (int i = 1; i < spectrum.Length - 1; i++)
+            //{
+            //    if (spectrum[i - 1] > max)
+            //        max = spectrum[i - 1];
+            //
+            //
+            //
+            //    Debug.DrawLine(new Vector3(i - 1, spectrum[i] + 10, 0), new Vector3(i, spectrum[i + 1] + 10, 0), Color.red,5);
+            //    Debug.DrawLine(new Vector3(i - 1, Mathf.Log(spectrum[i - 1]) + 10, 2), new Vector3(i, Mathf.Log(spectrum[i]) + 10, 2), Color.cyan,5);
+            //    Debug.DrawLine(new Vector3(Mathf.Log(i - 1), spectrum[i - 1] - 10, 1), new Vector3(Mathf.Log(i), spectrum[i] - 10, 1), Color.green,5);
+            //    Debug.DrawLine(new Vector3(Mathf.Log(i - 1), Mathf.Log(spectrum[i - 1]), 3), new Vector3(Mathf.Log(i), Mathf.Log(spectrum[i]), 3), Color.blue,5);
+            //}
+            //print(max);
+            if (didOnce == false)
             {
-                Debug.DrawLine(new Vector3(i - 1, spectrum[i] + 10, 0), new Vector3(i, spectrum[i + 1] + 10, 0), Color.red);
-                Debug.DrawLine(new Vector3(i - 1, Mathf.Log(spectrum[i - 1]) + 10, 2), new Vector3(i, Mathf.Log(spectrum[i]) + 10, 2), Color.cyan);
-                Debug.DrawLine(new Vector3(Mathf.Log(i - 1), spectrum[i - 1] - 10, 1), new Vector3(Mathf.Log(i), spectrum[i] - 10, 1), Color.green);
-                Debug.DrawLine(new Vector3(Mathf.Log(i - 1), Mathf.Log(spectrum[i - 1]), 3), new Vector3(Mathf.Log(i), Mathf.Log(spectrum[i]), 3), Color.blue);
+                InvokeRepeating("Check", 0, 1);
+                didOnce = true;
             }
         }
     }
+
+    private void Check()
+    {
+        float[] spectrum = new float[256];
+        GetComponent<AudioSource>().GetSpectrumData(spectrum, 0, FFTWindow.Rectangular);
+        float max = 0;
+        for (int i = 0; i < spectrum.Length; i++)
+        {
+            if (spectrum[i] > max)
+                max = spectrum[i - 1];
+        }
+        print(max*10000);
+    }
+
 }
