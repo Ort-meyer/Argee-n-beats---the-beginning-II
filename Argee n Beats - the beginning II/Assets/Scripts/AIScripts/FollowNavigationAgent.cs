b97a49c6ccabc59ae7a085b36d01db4abfe9678e
Akerspoket <konrad_o_92@hotmail.com>
2017-04-07 21:15:59 +0200

    Script for following a navigation object

---
+++ Argee n Beats - the beginning II/Assets/Scripts/AIScripts/FollowNavigationAgent.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowNavigationAgent : MonoBehaviour {
+    public GameObject navigation;
+    public float acceleration; // Units/second
+    private Vector3 totalForce;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // Find the direction vector
+        Vector3 direction = navigation.transform.position - this.transform.position;
+        float distance = direction.magnitude;
+        direction.Normalize();
+        if (direction != Vector3.zero)
+        {
+            totalForce += direction * acceleration * Time.deltaTime;
+        }
+
+	}
+
+    void FixedUpdate()
+    {
+        GetComponent<Rigidbody>().AddForce(totalForce, ForceMode.Acceleration);
+    }
+}
