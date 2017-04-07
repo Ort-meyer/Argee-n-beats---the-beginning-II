1bd0d530b1ccfe215b17bbc80f297db4365a05b3
Jawsarn <christian_markowicz@hotmail.com>
2017-04-07 21:15:22 +0200

    SoundChanger

--- Argee n Beats - the beginning II/Assets/Scripts/SoundChange.cs
+++ Argee n Beats - the beginning II/Assets/Scripts/SoundChange.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class SoundChange : MonoBehaviour {
 
+    RenderTexture renderTexture;
+    Texture2D texture;
+
 	// Use this for initialization
 	void Start () {
-		
-	}
+        //renderTexture = new RenderTexture(32, 32, 0, RenderTextureFormat.ARGB32);
+        //renderTexture.Create();
+        texture = new Texture2D(32,32);
+        GetComponent<Renderer>().material.mainTexture = texture;
+    }
 	
 	// Update is called once per frame
 	void Update () {
