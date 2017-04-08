eb05e50e7e7a7d9488cb5b0ad58799ea38a0f98d
Fetdressing <simon.home@hotmail.com>
2017-04-08 13:17:07 +0200

    moving platform with sound

---
+++ Argee n Beats - the beginning II/Assets/Scripts/PuzzleScripts/PlatformGonner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformGonner : MonoBehaviour {
+    public Rigidbody o_rigidbody;
+    public float force = 100;
+
+    public Transform directionObject;
+	// Use this for initialization
+	void Start () {
+		if(o_rigidbody == null)
+        {
+            o_rigidbody = GetComponent<Rigidbody>();
+        }
+	}
+
+    public void FlyAway()
+    {
+        o_rigidbody.isKinematic = false;
+        foreach(Collider col in o_rigidbody.transform.GetComponentsInChildren<Collider>())
+        {
+            col.isTrigger = true;
+        }
+
+        Vector3 dir = directionObject.position - o_rigidbody.transform.position.normalized;
+
+        o_rigidbody.AddForce(force * dir, ForceMode.Impulse);
+    }
+}
---
+++ Argee n Beats - the beginning II/Assets/Scripts/PuzzleScripts/PlatformLinearPath.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformLinearPath : MonoBehaviour {
+    public Rigidbody o_rigidbody;
+    public float speedF = 100;
+
+    public Transform[] paths_t;
+    Transform currPath = null;
+	// Use this for initialization
+	void Start () {
+        if (o_rigidbody == null)
+        {
+            o_rigidbody = GetComponent<Rigidbody>();
+        }
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        o_rigidbody.MovePosition( Vector3.Lerp(o_rigidbody.position, currPath.position, Time.fixedDeltaTime * speedF));
+	}
+
+    public void MoveTo(int index)
+    {
+        currPath = paths_t[index];
+    }
+}
