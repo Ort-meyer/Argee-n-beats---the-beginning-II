84265b2bc340a260e36677354a1fc78b7e4da952
asfasdasd <asda>
2017-04-08 15:25:37 +0200

    ändrade nog inget. fugnerar och vi har inge raidus som den måste vara innanför

--- Argee n Beats - the beginning II/Assets/DymanicObjectScript.cs
+++ Argee n Beats - the beginning II/Assets/DymanicObjectScript.cs
@@ -11,7 +11,7 @@ public class DymanicObjectScript : MonoBehaviour {
     //Gigantisk för att den alltid ska vara större än något
     Vector3 m_prevVecBetween = new Vector3(10000, 10000, 10000);
     float m_gravForce = 1.0f;
-    float m_radius = 5.0f;
+    public float m_radius = 5.0f;
 
     // Use this for initialization
     void Start ()
@@ -33,8 +33,8 @@ public class DymanicObjectScript : MonoBehaviour {
         {
                 Vector3 t_vectorBetween = -1*(gameObject.transform.position - t_targetTransform.position);
                 Vector3 t_crossResultNorm = Vector3.Cross(t_targetTransform.up, t_vectorBetween).normalized;
-            m_goalPos = t_crossResultNorm * 2 + t_targetTransform.position;
-            if(t_vectorBetween.magnitude > m_prevVecBetween.magnitude)
+            m_goalPos = t_crossResultNorm * m_radius + t_targetTransform.position;
+            if(t_vectorBetween.magnitude > m_prevVecBetween.magnitude)//|| t_vectorBetween.magnitude < m_radius)
             {
                 m_enterOrbit = true;
                 m_gravForce = Mathf.Pow(gameObject.GetComponent<Rigidbody>().velocity.magnitude, 2f) / t_vectorBetween.magnitude;
@@ -51,8 +51,13 @@ public class DymanicObjectScript : MonoBehaviour {
                 //gameObject.GetComponent<Rigidbody>().AddForce((GameObject.Find("GoalTarget").GetComponent<Transform>().position - gameObject.transform.position) * gameObject.GetComponent<Rigidbody>().velocity.magnitude*2);
                 if(t_vectorBetween.magnitude<m_radius)
                 {
-                    print(-1 * (t_targetTransform.position - gameObject.transform.position) * (Mathf.Log10(t_vectorBetween.magnitude / m_radius)) * 100);
-                    gameObject.GetComponent<Rigidbody>().AddForce(1*(t_targetTransform.position - gameObject.transform.position) *(Mathf.Log10(t_vectorBetween.magnitude/m_radius))*100);
+                    print(t_vectorBetween.magnitude);
+                    //print(t_vectorBetween.magnitude / m_radius);
+                    //print((Mathf.Log10(t_vectorBetween.magnitude / m_radius)));
+                    //print((1 * (t_targetTransform.position - gameObject.transform.position) * (Mathf.Log10(t_vectorBetween.magnitude / m_radius)) * 1000).magnitude);
+                    //gameObject.GetComponent<Rigidbody>().AddForce((gameObject.transform.position - t_targetTransform.position) * (-1*Mathf.Log10(t_vectorBetween.magnitude/m_radius)));
+                    //den bragameObject.GetComponent<Rigidbody>().AddForce(1*(t_targetTransform.position - gameObject.transform.position) * (Mathf.Log10(t_vectorBetween.magnitude/m_radius))*150);
+                    gameObject.GetComponent<Rigidbody>().AddForce((t_targetTransform.position - gameObject.transform.position) * gameObject.GetComponent<Rigidbody>().velocity.magnitude*2);
                     //gameObject.GetComponent<Rigidbody>().AddForce((t_targetTransform.position - gameObject.transform.position) * gameObject.GetComponent<Rigidbody>().velocity.magnitude*2);
                 }
                 else
