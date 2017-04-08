006ce1a3225bb0e7707141fd5350fabf756e7c3c
Akerspoket <konrad_o_92@hotmail.com>
2017-04-08 12:08:12 +0200

    Added some more

---
+++ Argee n Beats - the beginning II/Assets/Scripts/Health.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour {
+    public int startingHealth;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void TakeDamage(int damage)
+    {
+        startingHealth -= damage;
+        if (startingHealth <= 0)
+        {
+            Destroy(this);
+        }
+    }
+}
---
+++ Argee n Beats - the beginning II/Assets/Scripts/TakeDamageOnImpact.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TakeDamageOnImpact : MonoBehaviour {
+    public float imapctSpeedThreshold;
+    [Tooltip("Per speed unit over impact speed")]
+    public float damage;
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        
+    }
+}
