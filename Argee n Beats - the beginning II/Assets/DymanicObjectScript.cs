fd73b25c302986eff3b7284cbbbcde5b2ba9c59e
asfasdasd <asda>
2017-04-08 14:21:59 +0200

    Added orbit scriptgrejer så de flyger runt objektet.

---
+++ Argee n Beats - the beginning II/Assets/DymanicObjectScript.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DymanicObjectScript : MonoBehaviour {
+
+    OrbitGameobjectScript orbiScript;
+    Vector3 m_goalPos;
+    bool m_needToSetGoalPos = true;
+    bool m_enterOrbit = false;
+    //Gigantisk för att den alltid ska vara större än något
+    Vector3 m_prevVecBetween = new Vector3(10000, 10000, 10000);
+    float m_gravForce = 1.0f;
+    float m_radius = 5.0f;
+
+    // Use this for initialization
+    void Start ()
+    {
+        
+    }
+
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        ////Kollar alla spelarna om Co-op skulle vara en grej
+        //for (int i = 0; i < length; i++)
+        //{
+        // Om spelaren använder kraften att dra åt sig saker i orbit.
+        orbiScript = (OrbitGameobjectScript)GameObject.Find("PlayerCaps").GetComponent("OrbitGameobjectScript");
+        Transform t_targetTransform = GameObject.Find("PlayerCaps").GetComponent<Transform>();
+        if (orbiScript.m_usingDragAbility)
+        {
+                Vector3 t_vectorBetween = -1*(gameObject.transform.position - t_targetTransform.position);
+                Vector3 t_crossResultNorm = Vector3.Cross(t_targetTransform.up, t_vectorBetween).normalized;
+            m_goalPos = t_crossResultNorm * 2 + t_targetTransform.position;
+            if(t_vectorBetween.magnitude > m_prevVecBetween.magnitude)
+            {
+                m_enterOrbit = true;
+                m_gravForce = Mathf.Pow(gameObject.GetComponent<Rigidbody>().velocity.magnitude, 2f) / t_vectorBetween.magnitude;
+
+            }
+            if(m_enterOrbit)
+            {
+                //Kontrollera orbit. försöka
+                // typ reducera kraften på spheren.
+                // sätt ny target vecotr.
+                //float t_gravForce = Mathf.Pow(gameObject.GetComponent<Rigidbody>().velocity.magnitude,2)/ t_vectorBetween.magnitude;
+                //GameObject.Find("GoalTarget").GetComponent<Transform>().position
+
+                //gameObject.GetComponent<Rigidbody>().AddForce((GameObject.Find("GoalTarget").GetComponent<Transform>().position - gameObject.transform.position) * gameObject.GetComponent<Rigidbody>().velocity.magnitude*2);
+                if(t_vectorBetween.magnitude<m_radius)
+                {
+                    print(-1 * (t_targetTransform.position - gameObject.transform.position) * (Mathf.Log10(t_vectorBetween.magnitude / m_radius)) * 100);
+                    gameObject.GetComponent<Rigidbody>().AddForce(1*(t_targetTransform.position - gameObject.transform.position) *(Mathf.Log10(t_vectorBetween.magnitude/m_radius))*100);
+                    //gameObject.GetComponent<Rigidbody>().AddForce((t_targetTransform.position - gameObject.transform.position) * gameObject.GetComponent<Rigidbody>().velocity.magnitude*2);
+                }
+                else
+                {
+                    gameObject.GetComponent<Rigidbody>().AddForce((t_targetTransform.position - gameObject.transform.position) * gameObject.GetComponent<Rigidbody>().velocity.magnitude*2);
+                }
+
+            }
+            else
+            {
+                gameObject.GetComponent<Rigidbody>().AddForce((m_goalPos - gameObject.transform.position).normalized * 10);
+
+            }
+
+            m_prevVecBetween = t_vectorBetween;
+           // Debug.DrawLine(gameObject.transform.position, m_goalPos);
+
+        }
+        else
+        {
+            m_enterOrbit = false;
+        }
+        //}
+    }
+}
--- Argee n Beats - the beginning II/Assets/Scripts/OrbitGameobjectScript.cs
+++ Argee n Beats - the beginning II/Assets/Scripts/OrbitGameobjectScript.cs
@@ -4,8 +4,16 @@ using UnityEngine;
 
 public class OrbitGameobjectScript : MonoBehaviour {
 
+    
     // Use this for initialization
-	void Start () {
+    //float orbitRadiusCorrectionSpeed = 40.5f;
+    //float orbitRoationSpeed = 100.0f;
+    //float orbitRadius = 0.5f;
+    //float orbitAlignToDirectionSpeed = 10.5f;
+    //Vector3 previousPosition = new Vector3(10, 0, 0);
+    public bool m_usingDragAbility = true;
+
+    void Start () {
 
     }
 	
@@ -18,23 +26,39 @@ public class OrbitGameobjectScript : MonoBehaviour {
             GameObject t_gameObject = (GameObject)o;
             if(t_gameObject.tag == "DynamicObject")
             {
-                //Set target gameobject pull force towards this gameobject.
-                if(t_gameObject.GetComponent<Rigidbody>().velocity.magnitude<10)
-                {
-                    t_gameObject.GetComponent<Rigidbody>().AddForce((gameObject.transform.position-t_gameObject.GetComponent<Transform>().position)*10);
-                }
-            }
-            else if(t_gameObject.name == "OrbitControl")
-            {
-                //t_gameObject.GetComponent<Transform>.
-            }
-            //Transform t_tempTrans = GetComponentInChildren<Transform>();
+                /////Vector3 t_orbitAxis = new Vector3(0, 1, 0);
+                ///////Movement
+                /////t_gameObject.transform.RotateAround(gameObject.transform.position, t_orbitAxis, orbitRoationSpeed * Time.deltaTime);
+                /////Vector3 orbitDesiredPosition =(t_gameObject.transform.position - t_gameObject.transform.position).normalized * orbitRadius + t_gameObject.transform.position;
+                /////t_gameObject.transform.position = Vector3.Slerp(t_gameObject.transform.position, orbitDesiredPosition, Time.deltaTime * orbitRadiusCorrectionSpeed);
+                /////
+                ///////Rotation
+                /////Vector3 relativePos = t_gameObject.transform.position - previousPosition;
+                /////Quaternion rotation = Quaternion.LookRotation(relativePos);
+                /////t_gameObject.transform.rotation = Quaternion.Slerp(t_gameObject.transform.rotation, rotation, orbitAlignToDirectionSpeed * Time.deltaTime);
+                /////previousPosition = t_gameObject.transform.position;
 
+
+                //Set target gameobject pull force towards this gameobject.
+                //if (t_gameObject.GetComponent<Rigidbody>().velocity.magnitude<10)
+               // {
+                    ////////////t_gameObject.GetComponent<Rigidbody>().AddForce((gameObject.transform.position-t_gameObject.GetComponent<Transform>().position).normalized*400);
+                //}
+                //else
+                //{
+                //    t_gameObject.GetComponent<Rigidbody>().AddForce((gameObject.transform.position - t_gameObject.GetComponent<Transform>().position));
+                //}
+            }
+        
+        
         }
-        //print(gameObject.transform.GetChild(0).name);
-        //transform
-        /*Transform t_tempTrans =*/ //gameObject.transform.GetChild(0).transform.RotateAround(gameObject.transform.position, 10f);
-        //gameObject.transform.position += new Vector3(0,0, 0.1f);
+
+
+        //print("GubbPOS" + gameObject.transform.position);
+        //print("BOLLPOS" + gameObject.transform.GetChild(0).position);
+        //Vector3 tempRotateTrans = gameObject.transform.GetChild(0).position - transform.position; 
+        //tempRotateTrans = Quaternion.Euler(0,10 * Time.deltaTime,0) * tempRotateTrans;
+        //gameObject.transform.GetChild(0).transform.position = tempRotateTrans;
     }
 
 }
