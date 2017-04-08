6fdd4004644da3c9513665161e60e02ae779958e
Acc <Acc>
2017-04-08 16:26:32 +0200

    pre push commit. WOrk in progress

--- Argee n Beats - the beginning II/Assets/ShooterPuller.cs
+++ Argee n Beats - the beginning II/Assets/ShooterPuller.cs
@@ -2,15 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ShooterPuller : MonoBehaviour {
+public class ShooterPuller : MonoBehaviour
+{
+    public float m_maxFrequency = 1000;
+    public float m_fireArc = 0.1;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FrequencyAnalysis freqAnalys = GetComponent<FrequencyAnalysis>();
+        float fireFactor = freqAnalys.m_currentFrequency / m_maxFrequency;
+        fireFactor -= 0.5f;
+        fireFactor *= 2;
+
+        GameObject[] movables = GameObject.FindGameObjectsWithTag("DynamicObject");
+        foreach (GameObject obj in movables)
+        {
+
+        }
+    }
 }
