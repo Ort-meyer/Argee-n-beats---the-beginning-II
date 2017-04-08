e1b215bf75d60983b0d70e2bb1fed9af66523a92
Acc <Acc>
2017-04-08 16:37:06 +0200

    pusher done sortof

--- Argee n Beats - the beginning II/Assets/ShooterPuller.cs
+++ Argee n Beats - the beginning II/Assets/ShooterPuller.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class ShooterPuller : MonoBehaviour
 {
     public float m_maxFrequency = 1000;
-    public float m_fireArc = 0.1;
+    public float m_fireArc = 0.1f;
+    public float m_fireForce = 100;
 
     // Use this for initialization
     void Start()
@@ -16,14 +17,23 @@ public class ShooterPuller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        FrequencyAnalysis freqAnalys = GetComponent<FrequencyAnalysis>();
-        float fireFactor = freqAnalys.m_currentFrequency / m_maxFrequency;
-        fireFactor -= 0.5f;
-        fireFactor *= 2;
+        //FrequencyAnalysis freqAnalys = GetComponent<FrequencyAnalysis>();
+        //float fireFactor = freqAnalys.m_currentFrequency / m_maxFrequency;
+        //fireFactor -= 0.5f;
+       // fireFactor *= 2;
 
         GameObject[] movables = GameObject.FindGameObjectsWithTag("DynamicObject");
         foreach (GameObject obj in movables)
         {
+            Vector3 lineBetween = (obj.transform.position - transform.position).normalized;
+            Vector3 cameraTarget = Camera.current.transform.forward.normalized;
+
+            float dot = Vector3.Dot(lineBetween, cameraTarget);
+            Debug.Log(dot);
+            if (Vector3.Dot(lineBetween, cameraTarget) > 1-m_fireArc)
+            {
+                obj.GetComponent<Rigidbody>().AddForce(lineBetween * m_fireForce);
+            }
 
         }
     }
