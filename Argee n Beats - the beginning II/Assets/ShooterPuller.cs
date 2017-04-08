a7e70e639c3ce0506c463bbbdd42c76b73e2f311
Acc <Acc>
2017-04-08 18:05:41 +0200

    more tweaking with fire thingy

--- Argee n Beats - the beginning II/Assets/ShooterPuller.cs
+++ Argee n Beats - the beginning II/Assets/ShooterPuller.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class ShooterPuller : MonoBehaviour
 {
     public float m_maxFrequency = 700;
+    public float m_threshold = 300;
     public float m_fireArc = 0.1f;
     public float m_fireForce = 30;
 
@@ -18,10 +19,10 @@ public class ShooterPuller : MonoBehaviour
     void Update()
     {
         FrequencyAnalysis freqAnalys = GetComponent<FrequencyAnalysis>();
-        float fireFactor = freqAnalys.m_currentFrequency / m_maxFrequency;
-        fireFactor -= 0.5f;
-        fireFactor *= 2;
-        Debug.Log(fireFactor);
+        //float fireFactor = freqAnalys.m_currentFrequency / m_maxFrequency;
+        //fireFactor -= 0.5f;
+        //fireFactor *= 2;
+        //Debug.Log(fireFactor);
         //Debug.Log(freqAnalys.m_currentFrequency);
 
         GameObject[] movables = GameObject.FindGameObjectsWithTag("DynamicObject");
@@ -30,12 +31,24 @@ public class ShooterPuller : MonoBehaviour
             Vector3 lineBetween = (obj.transform.position - transform.position).normalized;
             Vector3 cameraTarget = Camera.current.transform.forward.normalized;
 
+            int fireFactor = 0;
             float dot = Vector3.Dot(lineBetween, cameraTarget);
             //Debug.Log(dot);
+            if (freqAnalys.m_currentFrequency > m_threshold)
+            {
+                Debug.Log("high");
+                fireFactor = 1;
+            }
+            else if (freqAnalys.m_currentFrequency <= m_threshold && freqAnalys.m_currentFrequency > 40)
+            {
+                Debug.Log("low");
+                fireFactor = -1;
+            }
+
             if (Vector3.Dot(lineBetween, cameraTarget) > 1-m_fireArc)
             {
                 if(freqAnalys.m_currentFrequency > 40)
-                obj.GetComponent<Rigidbody>().AddForce(lineBetween * m_fireForce * Mathf.Sign(fireFactor));
+                obj.GetComponent<Rigidbody>().AddForce(lineBetween * m_fireForce * fireFactor);
             }
 
         }
