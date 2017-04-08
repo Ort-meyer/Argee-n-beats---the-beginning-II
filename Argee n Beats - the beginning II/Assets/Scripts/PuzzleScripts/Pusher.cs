fc9c91fa7a1cdb25d399790cc685677fddc03391
Fetdressing <simon.home@hotmail.com>
2017-04-08 15:00:45 +0200

    pusher sound

--- Argee n Beats - the beginning II/Assets/Scripts/PuzzleScripts/Pusher.cs
+++ Argee n Beats - the beginning II/Assets/Scripts/PuzzleScripts/Pusher.cs
@@ -3,14 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Pusher : MonoBehaviour {
+    [System.NonSerialized]
+    public bool activated = false;
 
+    public float pushForce = 100;
+    public float maxVel = 100;
 	// Use this for initialization
 	void Start () {
-		
+        activated = false;
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    void OnTriggerStay(Collider c)
+    {
+        if (!activated)
+            return;
+
+        Rigidbody o_r = c.GetComponent<Rigidbody>();
+
+        if(o_r != null)
+        {
+            o_r.AddForce(transform.up * pushForce * Time.deltaTime, ForceMode.Force);
+        }
+    }
+
+    public void Toggle(bool b)
+    {
+        activated = b;
+    }
 }
