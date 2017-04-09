4e94a2d1ac8543dfdab93cd2b52d7a25001de1d1
Jawsarn <christian_markowicz@hotmail.com>
2017-04-09 13:20:29 +0200

    Particle trail

---
+++ Argee n Beats - the beginning II/Assets/Scripts/ParticleTrailManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleTrailManager : MonoBehaviour {
+
+    Rigidbody rig;
+    public Vector3 velocity;
+    Vector3 lastPos;
+
+    // Use this for initialization
+    void Start () {
+        rig = GetComponent<Rigidbody>();
+        lastPos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        int numchild = transform.childCount;
+            
+        for (int i = 0; i < numchild; i++)
+        {
+            TrailRenderer trailRend = transform.GetChild(i).GetComponent<TrailRenderer>();
+            if (trailRend && rig.velocity.magnitude > 0.0f)
+            {
+                Vector3 norm = trailRend.transform.position - transform.position;
+
+                float dotVal = Vector3.Dot(norm, rig.velocity.normalized);
+
+                if (Mathf.Abs(dotVal) < 0.5f )
+                {
+                    trailRend.time = (0.5f - Mathf.Abs(dotVal))*2.0f;
+                }
+                else
+                {
+                    trailRend.time = 0;
+                }
+            }
+            else if(trailRend)
+            {
+                trailRend.time *= 0.95f;
+            }
+        }
+	}
+}
