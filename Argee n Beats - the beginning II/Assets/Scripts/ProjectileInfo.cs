856677594ec81ff9f11c526a8988bd7b9ffd1c19
Akerspoket <konrad_o_92@hotmail.com>
2017-04-09 16:29:30 +0200

    Added a projectile info to modify specific projectiles that can be picked up

---
+++ Argee n Beats - the beginning II/Assets/Scripts/ProjectileInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileInfo : MonoBehaviour {
+    public float impulse;
+    public float damage;
+    public float radius;
+    public float damageRadius;
+    public GameObject explosionPrefab = null;
+}
