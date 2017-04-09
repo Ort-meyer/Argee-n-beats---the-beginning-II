f3090f12736e94dd8d9459c3a00025041dd8f89b
asfasdasd <asda>
2017-04-09 13:29:29 +0200

    Added beta för att skjuta iväg grejer. prototyp

--- Argee n Beats - the beginning II/Assets/DymanicObjectScript.cs
+++ Argee n Beats - the beginning II/Assets/DymanicObjectScript.cs
@@ -7,8 +7,15 @@ public class DymanicObjectScript : MonoBehaviour {
     OrbitGameobjectScript orbiScript;
     Vector3 m_goalPos;
     bool m_needToSetGoalPos = true;
+
+    //Här sätts den när den flyger iväg så man itne drar in den igen. Får sätta den när den träffar något eller vad simon tycker
+    public bool m_targetableForPower = true;
+    //Är det dags för den att entra orbit.
     bool m_enterOrbit = false;
-    bool m_inOrbit = false;
+    //Om den är i orbit och kan skjutas iväg. Om den är tillräcklgit nära spelaren.
+    public bool m_inOrbit = false;
+    //Sätter denna från ett annat script så den skjuts åt den riktningen hela tiden.
+    public bool m_addingForceToIt = false;
     //Gigantisk för att den alltid ska vara större än något
     Vector3 m_prevVecBetween = new Vector3(10000, 10000, 10000);
     float m_gravForce = 1.0f;
@@ -39,71 +46,78 @@ public class DymanicObjectScript : MonoBehaviour {
         Vector3 t_crossResultNorm = Vector3.Cross(t_targetTransform.up, t_vectorBetween).normalized;
         m_goalPos = t_crossResultNorm * m_radius + t_targetTransform.position;
 
-        if (orbiScript.m_usingDragAbility)
+        if (m_targetableForPower)
         {
-            if(t_vectorBetween.magnitude > m_prevVecBetween.magnitude)//|| t_vectorBetween.magnitude < m_radius)
-            {
-                m_enterOrbit = true;
-                m_gravForce = Mathf.Pow(gameObject.GetComponent<Rigidbody>().velocity.magnitude, 2f) / t_vectorBetween.magnitude;
-            }
 
-            if (m_enterOrbit)
+
+            if (orbiScript.m_usingDragAbility)
             {
-                if (t_vectorBetween.magnitude<m_radius)
+                if (t_vectorBetween.magnitude > m_prevVecBetween.magnitude)//|| t_vectorBetween.magnitude < m_radius)
                 {
-                    m_enterOrbit = false;
-             
-                    t_totalForce += (m_goalPos - gameObject.transform.position) * gameObject.GetComponent<Rigidbody>().velocity.magnitude * m_veloMultiplier;
-                    // den nya bra 3,0t_totalForce += (t_targetTransform.position - gameObject.transform.position) * gameObject.GetComponent<Rigidbody>().velocity.magnitude * m_veloMultiplier;
-                    //DEN nyabra 2.0gameObject.GetComponent<Rigidbody>().AddForce((t_targetTransform.position - gameObject.transform.position) * gameObject.GetComponent<Rigidbody>().velocity.magnitude* m_veloMultiplier);
-              
+                    m_enterOrbit = true;
+                    m_gravForce = Mathf.Pow(gameObject.GetComponent<Rigidbody>().velocity.magnitude, 2f) / t_vectorBetween.magnitude;
+                }
+
+                if (m_enterOrbit)
+                {
+                    if (t_vectorBetween.magnitude < m_radius)
+                    {
+                        m_enterOrbit = false;
+
+                        t_totalForce += (m_goalPos - gameObject.transform.position) * gameObject.GetComponent<Rigidbody>().velocity.magnitude * m_veloMultiplier;
+                        // den nya bra 3,0t_totalForce += (t_targetTransform.position - gameObject.transform.position) * gameObject.GetComponent<Rigidbody>().velocity.magnitude * m_veloMultiplier;
+                        //DEN nyabra 2.0gameObject.GetComponent<Rigidbody>().AddForce((t_targetTransform.position - gameObject.transform.position) * gameObject.GetComponent<Rigidbody>().velocity.magnitude* m_veloMultiplier);
+
+                    }
+                    else
+                    {
+                        t_totalForce += (m_goalPos - gameObject.transform.position) * gameObject.GetComponent<Rigidbody>().velocity.magnitude * m_veloMultiplier;
+                        m_enterOrbit = true;
+                    }
+
                 }
                 else
                 {
-                    t_totalForce += (m_goalPos - gameObject.transform.position) * gameObject.GetComponent<Rigidbody>().velocity.magnitude * m_veloMultiplier;
-                   m_enterOrbit = true;
+                    t_totalForce += (m_goalPos - gameObject.transform.position) * m_veloMultiplier;
+
                 }
 
+                m_prevVecBetween = t_vectorBetween;
             }
             else
             {
-                t_totalForce += (m_goalPos - gameObject.transform.position) * m_veloMultiplier;
-
+                m_enterOrbit = false;
             }
-
-            m_prevVecBetween = t_vectorBetween;
-        }
-        else
-        {
-            m_enterOrbit = false;
-        }
-        if ((gameObject.GetComponent<Rigidbody>().velocity - t_targetRigid.velocity).magnitude > m_veloMaxSpeed)
+            if ((gameObject.GetComponent<Rigidbody>().velocity - t_targetRigid.velocity).magnitude > m_veloMaxSpeed)
             {
-            float t_veloFactor = (gameObject.GetComponent<Rigidbody>().velocity - t_targetRigid.velocity).magnitude / m_veloMaxSpeed;
-            t_totalForce += -1 * (gameObject.GetComponent<Rigidbody>().velocity);
-        }
-        float t_distBetween = Vector3.Distance(gameObject.transform.position, t_targetTransform.position);
-        if (t_distBetween <= m_radiusForOutPusher)
-        {
-            float t_asd = 1 - (t_distBetween / m_radiusForOutPusher);
-            ////gameObject.GetComponent<Rigidbody>().velocity.normalized
-            ////Vector3 t_splitVelo = gameObject.GetComponent<Rigidbody>().velocity;
-            ////gameObject.GetComponent<Rigidbody>().velocity = t_splitVelo - Vector3.Dot(t_vectorBetween.normalized, t_splitVelo) * t_vectorBetween.normalized;
-            t_totalForce -= (t_targetTransform.position - gameObject.transform.position).normalized * 5000 * t_asd;
-        }
-        if(gameObject.GetComponent<Rigidbody>().velocity.magnitude > m_veloMaxSpeed)
-        {
-            gameObject.GetComponent<Rigidbody>().velocity = gameObject.GetComponent<Rigidbody>().velocity.normalized * m_veloMaxSpeed;
-        }
+                float t_veloFactor = (gameObject.GetComponent<Rigidbody>().velocity - t_targetRigid.velocity).magnitude / m_veloMaxSpeed;
+                t_totalForce += -1 * (gameObject.GetComponent<Rigidbody>().velocity);
+            }
+            float t_distBetween = Vector3.Distance(gameObject.transform.position, t_targetTransform.position);
+            if (t_distBetween <= m_radiusForOutPusher)
+            {
+                float t_asd = 1 - (t_distBetween / m_radiusForOutPusher);
+                ////gameObject.GetComponent<Rigidbody>().velocity.normalized
+                ////Vector3 t_splitVelo = gameObject.GetComponent<Rigidbody>().velocity;
+                ////gameObject.GetComponent<Rigidbody>().velocity = t_splitVelo - Vector3.Dot(t_vectorBetween.normalized, t_splitVelo) * t_vectorBetween.normalized;
+                t_totalForce -= (t_targetTransform.position - gameObject.transform.position).normalized * 5000 * t_asd;
+            }
+            if (gameObject.GetComponent<Rigidbody>().velocity.magnitude > m_veloMaxSpeed)
+            {
+                gameObject.GetComponent<Rigidbody>().velocity = gameObject.GetComponent<Rigidbody>().velocity.normalized * m_veloMaxSpeed;
+            }
             //print(-1 * (gameObject.GetComponent<Rigidbody>().velocity).magnitude);
             gameObject.GetComponent<Rigidbody>().AddForce(t_totalForce);
-        if(t_distBetween >= m_radiusForDetermineInOrbit)
-        {
-            m_inOrbit = true;
+            if (t_distBetween <= m_radiusForDetermineInOrbit)
+            {
+                m_inOrbit = true;
+            }
+            else
+            {
+                m_inOrbit = false;
+            }
         }
-        else
-        {
-            m_inOrbit = false;
-        }
+
+
     }
 }
--- Argee n Beats - the beginning II/Assets/OrbitShootScript.cs
+++ Argee n Beats - the beginning II/Assets/OrbitShootScript.cs
@@ -5,30 +5,56 @@ using UnityEngine;
 public class OrbitShootScript : MonoBehaviour {
 
     // Use this for initialization
+    DymanicObjectScript m_dynamicObjectScript;
     Ray m_ray;
     RaycastHit[] m_rayCastHits;
-	void Start () {
+    public float m_shootForce = 5000.0f;
+    public float m_shootCooldown = 1.0f;
+    float m_timer = 0.0f;
+    bool m_shotThisUpdate = false;
+
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
+        //Sätter den som false så vi kan skjuta denna update.
+        m_shotThisUpdate = false;
+        //uppdatera cooldowntimer
+        m_timer += Time.deltaTime;
         // Ta spelarens SiktarVector
         Transform t_targetTransform = GameObject.Find("PlaceHolderPlayerController").GetComponent<Transform>();
         //Kolla vilka som är framför gubben med en ray eller en fyrkant. 
         m_ray = new Ray(t_targetTransform.position, t_targetTransform.forward);
-        m_rayCastHits = Physics.RaycastAll(m_ray.origin, m_ray.direction, 20.0f);
+        m_rayCastHits = Physics.RaycastAll(m_ray.origin, m_ray.direction, 100.0f);
         int rayCastHitslength = m_rayCastHits.Length;
-        for (int i = 0; i < rayCastHitslength; i++)
+        if (m_shootCooldown < m_timer)
         {
-            if (m_rayCastHits[i].transform.gameObject.tag == "DynamicObject")
+            //Debug.DrawLine(m_ray.origin, m_ray.origin + m_ray.direction * 100.0f);
+
+            for (int i = 0; i < rayCastHitslength; i++)
             {
-                //print("Okejdå");
+                // Vill bräjka forloopen om vi har skjutit den här updaten
+                if (m_rayCastHits[i].transform.gameObject.tag == "DynamicObject")// && !m_shotThisUpdate)
+                {
+                    m_dynamicObjectScript = (DymanicObjectScript)m_rayCastHits[i].transform.gameObject.GetComponent("DymanicObjectScript");
+                    //Kolla om objektet är in orbit så vi kan använda det som projektil.
+                    if (m_dynamicObjectScript.m_inOrbit)// && m_dynamicObjectScript.m_targetableForPower)
+                    {
+                        m_timer = 0.0f;
+                        m_shotThisUpdate = true;
+                        m_dynamicObjectScript.m_targetableForPower = false;
+                        m_dynamicObjectScript.m_inOrbit = false;
+                        m_rayCastHits[i].transform.GetComponent<Rigidbody>().velocity = Vector3.zero;
+                        //Skjuta iväg den.
+                        m_rayCastHits[i].transform.GetComponent<Rigidbody>().AddForce(m_ray.direction * m_shootForce);
+                        print(m_ray.direction * m_shootForce);
+                    }
+                }
             }
         }
-        //Kolla om objektet är in orbit så vi kan använda det som projektil.
-        //Skjuta iväg den.
 
     }
 }
