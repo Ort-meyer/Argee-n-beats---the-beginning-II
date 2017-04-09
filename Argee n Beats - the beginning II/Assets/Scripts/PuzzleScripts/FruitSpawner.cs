3bcdaf716746123488c392363d870103bfc82d6b
Fetdressing <simon.home@hotmail.com>
2017-04-09 13:09:24 +0200

    meck med fruit dropper

--- Argee n Beats - the beginning II/Assets/Scripts/PuzzleScripts/FruitSpawner.cs
+++ Argee n Beats - the beginning II/Assets/Scripts/PuzzleScripts/FruitSpawner.cs
@@ -3,14 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class FruitSpawner : MonoBehaviour {
-
+    public Transform[] growers;
+    SoundChange sChange;
 	// Use this for initialization
 	void Start () {
-		
+        sChange = GetComponent<SoundChange>();
+
+        StartCoroutine(Grow());
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    IEnumerator Grow()
+    {
+        while(sChange.GetPercentageFilled() < 0.95f)
+        {
+            for(int i = 0; i < growers.Length; i++)
+            {
+                float value = sChange.GetPercentageFilled() + 1.0f + Random.Range(0.001f, 0.01f);
+                growers[i].localScale = new Vector3(value, value, value);
+            }
+            yield return new WaitForEndOfFrame();
+        }
+    }
 }
