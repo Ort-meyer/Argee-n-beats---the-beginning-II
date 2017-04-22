36f47ff8d26734ac5f68a0acdefedf6647e1883d
Jimmy <neuparpol@gmail.com>
2017-04-22 19:20:25 +0200

    har lagt till collectibles. i scriptmapen =)=)=)=D lägg till den på gubben så plockar den upp items med ett visst namn, just nu Pickup

---
+++ Argee n Beats - the beginning II/Assets/Scripts/CollectPickUps.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectPickUps : MonoBehaviour {
+    public int m_howManyCollectibles = 0;
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Pickup")
+        {
+            other.gameObject.SetActive(false);
+            m_howManyCollectibles++;
+
+        }
+    }
+}
