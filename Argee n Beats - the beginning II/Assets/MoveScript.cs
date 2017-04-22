412df295318ba80ba7cf1a1301ddc451f2ba1671
Jawsarn <christian_markowicz@hotmail.com>
2017-04-22 14:09:34 +0200

    Jaws

---
+++ Argee n Beats - the beginning II/Assets/MoveScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveScript : MonoBehaviour {
+    float val;
+	// Use this for initialization
+	void Start () {
+        val = transform.position.x;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (GetComponent<Rigidbody>().velocity.x < 2)
+        {
+            GetComponent<Rigidbody>().AddForce(new Vector3(1, 0, 0), ForceMode.VelocityChange);
+        }
+
+	}
+}
