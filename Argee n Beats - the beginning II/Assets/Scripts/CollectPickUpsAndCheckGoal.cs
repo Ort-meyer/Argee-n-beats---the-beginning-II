f21b13616e5b994ac8555737fc9f19e222bbdbf8
Jimmy <neuparpol@gmail.com>
2017-04-22 20:21:22 +0200

    la till goalcheck men ska fixa så att det blir något slut typ en canvas som dyker upp och ritar ut en bild där det står end of demo

---
+++ Argee n Beats - the beginning II/Assets/Scripts/CollectPickUpsAndCheckGoal.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectPickUpsAndCheckGoal : MonoBehaviour {
+    public int m_howManyCollectibles = 0;
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Pickup")
+        {
+            other.gameObject.SetActive(false);
+            m_howManyCollectibles++;
+
+        }
+        if (other.gameObject.name == "Goal" && m_howManyCollectibles >= 2)
+        {
+            print("Brajobbat");
+        }
+    }
+}
