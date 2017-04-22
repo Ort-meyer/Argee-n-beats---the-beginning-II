842de02ee657ccbc54a654c2b918621a878f3cef
Jawsarn <christian_markowicz@hotmail.com>
2017-04-22 18:41:28 +0200

    Deathzones

---
+++ Argee n Beats - the beginning II/Assets/Scripts/DeathZone.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DeathZone : MonoBehaviour
+{
+    GameObject youDiedObj;
+    // Use this for initialization
+    void Start()
+    {
+        youDiedObj = GameObject.Find("YOUDIED");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void DisableHealthGUI()
+    {
+        youDiedObj.gameObject.GetComponent<Text>().enabled = false;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Health healtComp = other.GetComponent<Health>();
+        if (healtComp)
+        {
+            healtComp.TakeDamage(healtComp.startingHealth);
+            youDiedObj.gameObject.GetComponent<Text>().enabled = true;
+            Invoke("DisableHealthGUI", 3.0f);
+        }
+    }
+}
+
