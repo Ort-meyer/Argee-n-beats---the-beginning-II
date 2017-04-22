d99fe7912addd2e07f2c3df56b6353fee2bc5a41
Konrad Öhman <konrad_o_92@hotmail.com>
2017-04-22 15:43:38 +0200

    Remote controlled script

---
+++ Argee n Beats - the beginning II/Assets/Scripts/RemoteControlledObject.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RemoteControlledObject : MonoBehaviour {
+    public Vector3 start;
+    public Vector3 stop;
+    public float speed;
+    float fromStart = 0;
+    bool triggered;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (triggered)
+        {
+            fromStart += speed * Time.deltaTime;
+            fromStart = Mathf.Min(1, fromStart);
+            transform.position = Vector3.Lerp(start, stop, fromStart);
+        }
+        else
+        {
+            fromStart -= speed * Time.deltaTime;
+            fromStart = Mathf.Max(0, fromStart);
+            transform.position = Vector3.Lerp(start, stop, fromStart);
+        }
+	}
+
+    public void Enter()
+    {
+        triggered = true;
+    }
+
+    public void Exit()
+    {
+        triggered = false;
+    }
+}
