3edfd1992e5ab1eb471d92263c4fecb9e9f501c0
Jawsarn <christian_markowicz@hotmail.com>
2017-04-22 20:27:26 +0200

    Jaws fixing tooltips

---
+++ Argee n Beats - the beginning II/Assets/Scripts/ToolTipCheckpoint.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToolTipCheckpoint : MonoBehaviour {
+
+    bool entered = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (entered == false)
+        {
+            entered = true;
+            transform.parent.GetComponent<ToolTipManager>().UpdateToolTip();
+        }
+
+    }
+}
--- Argee n Beats - the beginning II/Assets/Scripts/ToolTipManager.cs
+++ Argee n Beats - the beginning II/Assets/Scripts/ToolTipManager.cs
@@ -1,12 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ToolTipManager : MonoBehaviour {
 
+    List<GameObject> toolTips = new List<GameObject>();
+    int curToolTip = 0;
 	// Use this for initialization
 	void Start () {
-		
+        GameObject canvas = GameObject.Find("Canvas");
+        for (int i = 0; i < canvas.transform.childCount; i++)
+        {
+            GameObject child = canvas.transform.GetChild(i).gameObject;
+            if (child.name.Contains("TOOLTIP"))
+            {
+                toolTips.Add(child);
+            }
+        }
+
+        toolTips[0].GetComponent<Text>().enabled = true;
 	}
 	
 	// Update is called once per frame
@@ -14,8 +27,17 @@ public class ToolTipManager : MonoBehaviour {
 		
 	}
 
-    void OnTriggerEnter(Collider other)
+    public void UpdateToolTip()
     {
-        Debug.Log("TRIGGERD");
+        if (curToolTip < toolTips.Count)
+        {
+            toolTips[curToolTip].GetComponent<Text>().enabled = false;
+            curToolTip++;
+
+            if (curToolTip < toolTips.Count)
+            {
+                toolTips[curToolTip].GetComponent<Text>().enabled = true;
+            }
+        }
     }
 }
