11b88c01fc15d1f114d128366626f69078442379
Jawsarn <christian_markowicz@hotmail.com>
2017-04-23 12:46:44 +0200

    Fixes yo

--- Argee n Beats - the beginning II/Assets/Scripts/CollectPickUpsAndCheckGoal.cs
+++ Argee n Beats - the beginning II/Assets/Scripts/CollectPickUpsAndCheckGoal.cs
@@ -1,20 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CollectPickUpsAndCheckGoal : MonoBehaviour {
-    public int m_howManyCollectibles = 0;
+    int m_maxCollectibles = 0;
+    int m_howManyCollectibles = 0;
+
+    void Start()
+    {
+        GameObject[] collectebles = GameObject.FindGameObjectsWithTag("Pickups");
+        m_maxCollectibles = collectebles.Length;
+    }
+
     void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.name == "Pickup")
+        if (other.gameObject.tag == "Pickups")
         {
             other.gameObject.SetActive(false);
             m_howManyCollectibles++;
 
         }
-        if (other.gameObject.name == "Goal" && m_howManyCollectibles >= 2)
+        if (other.gameObject.tag == "Goal" && m_howManyCollectibles >= m_maxCollectibles)
         {
-            print("Brajobbat");
+            // output canvas stuff
+            GameObject canvas = GameObject.Find("Canvas");
+            for (int i = 0; i < canvas.transform.childCount; i++)
+            {
+                if (canvas.transform.GetChild(i).name.Equals("YOUWON"))
+                {
+                    canvas.transform.GetChild(i).GetComponent<Text>().enabled = true;
+                }
+            }
         }
     }
 }
