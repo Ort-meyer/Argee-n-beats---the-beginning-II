86aaa2fab26b36a2b593fdd894c89bd09dfbe165
Jimmy <neuparpol@gmail.com>
2017-04-23 13:32:14 +0200

    lagt till så kristallerna tar bort dess velocity när de respawnar

--- Argee n Beats - the beginning II/Assets/Scripts/RespawnNoHealth.cs
+++ Argee n Beats - the beginning II/Assets/Scripts/RespawnNoHealth.cs
@@ -6,14 +6,17 @@ public class RespawnNoHealth : MonoBehaviour {
 
     [HideInInspector]
     public Vector3 respawnPos;
+    Rigidbody rigidBody;
 
 	// Use this for initialization
 	void Start () {
         respawnPos = transform.position;
-	}
+        rigidBody = GetComponent<Rigidbody>();
+    }
 
     public void Respawn()
     {
         transform.position = respawnPos;
+        rigidBody.velocity = Vector3.zero;
     }
 }
